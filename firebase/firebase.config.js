f74bcea254a94b812d5dda8e3f9748589fac77fb
kaleytsangbcit <ktsang38@my.bcit.ca>
2022-11-07 12:06:00 -0800

    delete the id

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,17 +1,4 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from 'firebase/auth'
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCBxaalXOhpj5LxDIa7U1kmw2c6rxefutc",
-//   authDomain: "login-with-firebase-1102.firebaseapp.com",
-//   projectId: "login-with-firebase-1102",
-//   storageBucket: "login-with-firebase-1102.appspot.com",
-//   messagingSenderId: "111797970394",
-//   appId: "1:111797970394:web:de8e04ff79d69086fd0ce3"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
